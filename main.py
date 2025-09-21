bfe4a97729c9061eb584b1341984bb1c0c780a84
zuoyi34 <ngwo568@gmail.com>
2025-09-21 22:37:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
+        'https://vue-admin-custom-production.up.railway.app',
     ],
     allow_credentials=True,
     allow_methods=["*"],
