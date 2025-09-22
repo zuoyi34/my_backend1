4c644f4bea0295d02e24fb51203e87aa7a9a9907
zuoyi34 <ngwo568@gmail.com>
2025-09-22 22:26:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
         "https://beamish-arithmetic-21d959.netlify.app"
     ],
     allow_credentials=True,
