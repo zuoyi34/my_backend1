65ba055e1d658bad6d518d4e807137082fb9f80c
zuoyi34 <ngwo568@gmail.com>
2025-09-22 22:19:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
-        'https://vue-admin-custom-production.up.railway.app',
-         "http://localhost:4173/login"
+        "https://beamish-arithmetic-21d959.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
